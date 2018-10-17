3cfa4fdadaa0cc3cf03ec8e2d8ee7c22422a3894
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2018-10-17 10:41:36 +0200

    Added basic file structure
    The basics for the program, also one basic array to start off with.

---
+++ data.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+// This is the file where you can keep your data arrays such as articles and
+// authors.
+
+$authorsAndArticles = [
+  'Author1' => [
+      'Article1',
+      'Article2',
+  ],
+  'Author2' => [
+      'Article1',
+      'Article2',
+  ],
+  'Author3' => [
+      'Article1',
+      'Article2',
+  ],
+  'Author4' => [
+      'Article1',
+      'Article2',
+  ],
+  'Author5' => [
+      'Article1',
+      'Article2',
+  ],
+]
+
+?>
