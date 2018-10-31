834f3dca76dfa4c7e7a7291558f444a5d36d50f7
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2018-10-31 12:00:32 +0100

    Edited styles

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # FakeNews
+
+## Description
+Fake News
+
+
+## Installation
+
+1. Clone the directory
+    ```
+    git clone https://github.com/VincentLideboKjellman/FakeNews
+    ```
+
+
+## Testers
+
+Gustav Svendsen
+
+Benjamin Larsson
+
+
+
+## License
+
+The MIT License ([MIT](https://raw.githubusercontent.com/erhuz/Fake-News/master/LICENSE))
--- index.php
+++ index.php
@@ -18,61 +18,68 @@ usort($sortedArticles, "compareDate");
 //     }
 //   }
 // }
- ?>
+?>
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/styles.css" type="text/css">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title></title>
-  </head>
-  <body>
-    <!-- Page Content -->
-      <div class="container">
+<head>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/styles.css" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Cinzel|Montserrat" rel="stylesheet">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title></title>
+</head>
+<body>
 
-        <div class="row">
-          <div class="col-md-3"></div>
-          <!-- Blog Entries Column -->
+  <!--------------------------------------------------------------------->
+  <!-- Page Content -->
+  <!--------------------------------------------------------------------->
+  <div class="container">
 
-          <div class="col-md-6" align="center">
+    <div class="row">
+      <div class="col-md-3"></div>
+      <!-- Blog Entries Column -->
 
-            <h1 class="my-4">Fake News</h1>
-            <!-- Blog Post -->
-            <?php foreach ($sortedArticles as $article): ?>
-              <div class="card mb-4">
-                <img class="card-img-top" src="<?php echo $article['img']?>" alt="asd" width='750px' height='300px'>
-                <div class="card-body">
-                  <h2 class="card-title"><?php echo $article['title'];?></h2>
-                  <p class="card-text"><?php echo $article['content']; ?></p>
-                <!--a href="#" class="btn btn-primary">Read More &rarr;</a>-->
-                </div>
-              <div class="card-footer text-muted">
-                Posted on <?php echo $article['publisherDate'];?> by
-                <?php foreach ($authors as $author): ?>
-                  <?php if ($author['id'] === $article['authorId']): ?>
-                  <a href="#"><?php echo $author['name']  ?></a>
-                  <?php endif; ?>
-                <?php endforeach; ?>
-                <p>Likes: <?php echo $article['likeCounter'] ?> 💛</p>
-              </div>
+      <div class="col-md-6" align="center">
+
+        <h1 class="my-4">Fake News</h1>
+
+        <!--------------------------------------------------------------------->
+        <!-- Blog Post -->
+        <!--------------------------------------------------------------------->
+        <?php foreach ($sortedArticles as $article): ?>
+          <div class="card mb-4">
+            <img class="card-img-top" src="<?php echo $article['img']?>" alt="asd" width='750px' height='300px'>
+            <div class="card-body card-edit">
+              <h2 class="card-title"><?php echo $article['title'];?></h2>
+              <p class="card-text"><?php echo $article['content']; ?></p>
+              <!--a href="#" class="btn btn-primary">Read More &rarr;</a>-->
             </div>
-            <?php endforeach; ?>
+            <div class="card-footer text-muted">
+              Posted on <?php echo $article['publisherDate'];?> by
+              <?php foreach ($authors as $author): ?>
+                <?php if ($author['id'] === $article['authorId']): ?>
+                  <a href="#"><?php echo $author['name']  ?></a>
+                <?php endif; ?>
+              <?php endforeach; ?>
+              <p>Likes: <?php echo $article['likeCounter'] ?> 💛</p>
+            </div>
           </div>
-        </div>
-        <!-- /.row -->
-
+        <?php endforeach; ?>
       </div>
-      <!-- /.container -->
+    </div>
+    <!-- /.row -->
 
-      <!-- Footer -->
-      <footer class="py-5 bg-dark">
-        <div class="container">
-          <p class="m-0 text-center text-white">Copyright &copy; Fake News 2018</p>
-        </div>
-        <!-- /.container -->
-      </footer>
-  </body>
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Fake News 2018</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+</body>
 </html>
